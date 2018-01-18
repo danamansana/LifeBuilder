7f5d91bc861c2390dc275aac3c0abedf26f01474
danamansana <dan.s.appel@gmail.com>
2018-01-17 19:18:24 -0500

    can click grid elements on and off

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -4,17 +4,47 @@ class Grid extends React.Component {
 
   constructor(props){
     super(props);
+    let backGrid = [];
+    let backRow;
+    for(backRow = 0; backRow < 50; backRow++){
+      let backRowBox = [];
+      let backCol;
+      for (backCol = 0; backCol < 50; backCol++){
+        backRowBox.push([0]);
+      }
+      backGrid.push(backRowBox);
+    }
+    this.state = {backGrid: backGrid}
   }
 
   render(){
+    function flip(row, col){
+      return () => {
+      let backGrid = [];
+      let backRow;
+      for(backRow = 0; backRow < 50; backRow++){
+        let backRowBox = [];
+        let backCol;
+        for (backCol = 0; backCol < 50; backCol++){
+          if(row !== backRow || col !== backCol) {
+            backRowBox.push([this.state.backGrid[row][col]]);
+          } else {
+            backRowBox.push([1- this.state.backGrid[row][col]])
+          }
+        }
+        backGrid.push(backRowBox);
+      }
+      this.setState({backGrid: backGrid})
+    };
+  }
     let grid = [];
     let row;
     for(row = 0; row < 50; row++){
       let rowBox = [];
       let col;
       for(col = 0; col < 50; col++){
-        let name = ((row+col)%2===0 ? "even" : "odd")
-        rowBox.push(<div className={`${name}`}/>)
+        let name = (this.state.backGrid[row][col] === 0 ? "even" : "odd")
+        rowBox.push(<div className={`${name}`} onClick={flip(row,col)}/>)
       }
       grid.push(<div className='row'>{rowBox}</div>);
     }
--- life_builder.jsx
+++ life_builder.jsx
@@ -2,8 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 
+function toggleFunction(el) {
+  return () => {el.classList.toggle('odd');};
+}
+
 document.addEventListener('DOMContentLoaded', ()=> {
   console.log('here I am');
   const root = document.getElementById('root');
-  ReactDOM.render(<Root/>, root);
+  let header = document.createElement("div");
+  header.classList.add("header");
+  root.appendChild(header);
+  let middle = document.createElement("div");
+  middle.classList.add("middle");
+  root.appendChild(middle);
+  let options = document.createElement("div");
+  options.classList.add("options");
+  middle.appendChild(options);
+  let grid = document.createElement("div");
+  grid.classList.add("grid");
+  middle.appendChild(grid);
+  let row = 0;
+  for(row = 0; row < 50; row++){
+    let rowEl = document.createElement("div");
+    rowEl.classList.add("row");
+    grid.appendChild(rowEl);
+    let col = 0;
+    for(col = 0; col < 50; col++){
+      let colEl = document.createElement("div");
+      //colEl.classList.add(((row+col)%2===0 ? "even" : "odd"));
+      colEl.classList.add("odd");
+      colEl.classList.add("even");
+      
+      colEl.addEventListener("click", toggleFunction(colEl))
+      rowEl.appendChild(colEl);
+    }
+  }
+  let selections = document.createElement("div");
+  selections.classList.add("selections");
+  middle.appendChild(selections);
+  let footer = document.createElement("div");
+  footer.classList.add("footer");
+  root.appendChild(footer);
+
 });
